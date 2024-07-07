955c6bca0c61f4e364da26a097b1907d336394bd
xander <xanderelsmith@gmail.com>
2024-07-07 16:34:18 +0100

    referscreen card

--- lib/features/refer/presentation/pages/referscreen.dart
+++ lib/features/refer/presentation/pages/referscreen.dart
@@ -9,21 +9,67 @@ class ReferScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Invite Friends'),
+          title: const Text('Patients'),
         ),
         body: ListView.builder(
-          itemBuilder: (context, index) => ListTile(
-            leading: const CircleAvatar(),
-            title: const Text('Darrel Steward'),
-            subtitle: const Text('+234-9079985394'),
-            trailing: OutlinedButton(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20))),
-              child: const Text('Invite'),
-            ),
-          ),
+          itemCount: 4,
+          itemBuilder: (context, index) => Card(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+                height: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const PatientReferInfoText(
+                      title: 'Patient',
+                      info: 'Mr Francis Sam',
+                    ),
+                    const PatientReferInfoText(
+                      title: 'Doctor',
+                      info: 'Charles Devoir',
+                    ),
+                    const PatientReferInfoText(
+                      title: 'Date:',
+                      info: ' 13/3/2090',
+                    ),
+                    OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          fixedSize: const Size(double.maxFinite, 20),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                      child: const Text('View Summary'),
+                    ),
+                  ],
+                )),
+          )),
         ));
+  }
+}
+
+class PatientReferInfoText extends StatelessWidget {
+  const PatientReferInfoText({
+    super.key,
+    required this.title,
+    required this.info,
+  });
+  final String title;
+  final String info;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text(
+        title,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+        ),
+        child: Text(info),
+      ),
+    ]);
   }
 }
