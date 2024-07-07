5ce73aca12850e80a90ec1ce608e09d33aed4608
xander <xanderelsmith@gmail.com>
2024-07-07 16:42:35 +0100

    referscreen card

--- lib/features/refer/presentation/pages/referscreen.dart
+++ lib/features/refer/presentation/pages/referscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:health_proj/features/refer/presentation/widgets/patientreferinfotext.dart';
+import 'package:health_proj/features/refer/presentation/widgets/petientrefercard.dart';
 
 class ReferScreen extends StatelessWidget {
   const ReferScreen({
@@ -14,37 +15,7 @@ class ReferScreen extends StatelessWidget {
         ),
         body: ListView.builder(
           itemCount: 4,
-          itemBuilder: (context, index) => Card(
-              child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-                height: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const PatientReferInfoText(
-                      title: 'Patient',
-                      info: 'Mr Francis Sam',
-                    ),
-                    const PatientReferInfoText(
-                      title: 'Doctor',
-                      info: 'Charles Devoir',
-                    ),
-                    const PatientReferInfoText(
-                      title: 'Date:',
-                      info: ' 13/3/2090',
-                    ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      style: OutlinedButton.styleFrom(
-                          fixedSize: const Size(double.maxFinite, 20),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20))),
-                      child: const Text('View Summary'),
-                    ),
-                  ],
-                )),
-          )),
+          itemBuilder: (context, index) => const PatientReferCard(),
         ));
   }
 }
